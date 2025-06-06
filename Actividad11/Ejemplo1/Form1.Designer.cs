a91e7074d8cc6e01ecd0f71fe2d2b1e67584473b
Facundo Citera <facundojesus10@hotmail.com>
2025-06-06 08:44:49 -0300

    Ejemplo1

---
+++ Actividad11/Ejemplo1/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Ejemplo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnMostrar = new Button();
+            SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.BackColor = SystemColors.ActiveCaption;
+            btnMostrar.Location = new Point(232, 91);
+            btnMostrar.Margin = new Padding(3, 4, 3, 4);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(104, 53);
+            btnMostrar.TabIndex = 0;
+            btnMostrar.Text = "Mostrar";
+            btnMostrar.UseVisualStyleBackColor = false;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(607, 240);
+            Controls.Add(btnMostrar);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnMostrar;
+    }
+}
